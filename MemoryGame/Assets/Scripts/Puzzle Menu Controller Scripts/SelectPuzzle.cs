6456e2c1cb0b3be98c5b289e590c236d29aa854c
carlos <cacalder@cisco.com>
2016-04-10 17:54:06 -0500

    initial commit

---
+++ MemoryGame/Assets/Scripts/Puzzle Menu Controller Scripts/SelectPuzzle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPuzzle : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject selectPuzzleMenuPanel;
+
+    [SerializeField] private Animation selectPuzzleMenuAnim;
+
+    private string selectPuzzle;
+
+    public void SelectedPuzzle()
+    {
+        selectPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log(selectPuzzle);
+    }
+
+}
