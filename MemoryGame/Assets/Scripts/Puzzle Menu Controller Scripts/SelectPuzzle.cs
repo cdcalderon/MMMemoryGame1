85f976ece2924a9755aa0ae57a8129c10767d6fb
carlos <cacalder@cisco.com>
2016-04-14 00:19:34 -0500

    add select Puzzle Scripts

--- MemoryGame/Assets/Scripts/Puzzle Menu Controller Scripts/SelectPuzzle.cs
+++ MemoryGame/Assets/Scripts/Puzzle Menu Controller Scripts/SelectPuzzle.cs
@@ -5,16 +5,26 @@ public class SelectPuzzle : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject selectPuzzleMenuPanel;
+    private GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
 
-    [SerializeField] private Animation selectPuzzleMenuAnim;
+    [SerializeField] private Animator selectPuzzleMenuAnim, puzzleLevelSelectAnim;
 
     private string selectPuzzle;
 
     public void SelectedPuzzle()
     {
         selectPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        Debug.Log(selectPuzzle);
+
+        StartCoroutine(ShowPuzzleLevelSelectMenu());
+    }
+
+    IEnumerator ShowPuzzleLevelSelectMenu()
+    {
+        puzzleLevelSelectPanel.SetActive(true);
+        selectPuzzleMenuAnim.Play("SlideOut");
+        puzzleLevelSelectAnim.Play("SlideIn");
+        yield return new WaitForSeconds(1f);
+        selectPuzzleMenuPanel.SetActive(false);
     }
 
 }
